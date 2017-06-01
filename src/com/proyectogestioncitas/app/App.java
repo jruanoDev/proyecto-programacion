4e2421bb518c5853efda7e50af92a334e535a306
Jose Ruano <jruanogalvez@gmail.com>
2017-06-01 13:48:50 +0200

    modificado:    bin/com/proyectogestioncitas/app/App.class 	nuevo archivo: bin/com/proyectogestioncitas/model/Conexion.class 	nuevo archivo: config/dbConfig.xml 	modificado:    src/com/proyectogestioncitas/app/App.java 	nuevo archivo: src/com/proyectogestioncitas/model/Conexion.java
    	Añadido ejemplo de archivo de configuración XML y comenzada lógica
    	del inicio de la App.

--- src/com/proyectogestioncitas/app/App.java
+++ src/com/proyectogestioncitas/app/App.java
@@ -1,6 +1,80 @@
 package com.proyectogestioncitas.app;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class App {
 	
+	private String dbUrl;
+	private String dbUser;
+	private String dbPassword;
+	
+	// Al iniciar la app primero comprobamos el archivo XML, lo almacenaremos en una carpeta llamada config
+	// donde estará el archivo de configuración, si no existe lo creamos nosotros de nuevo
+	
+	public static void main(String[] args) {
+		new App();
+	}
+	
+	public App() {
+		File xmlConfigFile = new File("config/dbConfig.xml");
+		checkXMLFile(xmlConfigFile);
+	}
+	
+	public void checkXMLFile(File xmlFile) {
+		
+		if(xmlFile.exists()) {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			
+			try {
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(xmlFile);
+				
+				NodeList nList = doc.getElementsByTagName("user");
+				
+				Node nNode = nList.item(0);
+				
+				if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) nNode;
+					
+					dbUrl = element.getElementsByTagName("dburl").item(0).getTextContent();
+					dbUser = element.getElementsByTagName("dbuser").item(0).getTextContent();
+					dbPassword = element.getElementsByTagName("dbpassword").item(0).getTextContent();
+					
+					// Si todos los datos están correctos abriremos la conexión a la base de datos con todos
+					// estos datos recogidos del XML
+					
+					if(dbUrl == "none" || dbUser == "none" || dbPassword == "none") {
+						// Aquí abriremos la pantalla de configuración de la base de datos
+						
+					}
+					
+					
+					
+				}
+				
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
 	
 }
---
+++ src/com/proyectogestioncitas/model/Conexion.java
@@ -0,0 +1,38 @@
+package com.proyectogestioncitas.model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+	private static Connection conexion = null;
+	
+	// El constructor está modificado para estos parámetros recogidos del XML en App.java
+	private Conexion(String dbUrl, String dbUser, String dbPassword) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			//joseoski.heliohost.org
+			conexion = DriverManager.getConnection(dbUrl, 
+					dbUser, dbPassword);
+			
+			/* conexion = DriverManager.getConnection("jdbc:mysql://sql8.freesqldatabase.com:3306/sql8177637", 
+					"sql8177637", "li94WcskFU"); */
+			
+		} catch (SQLException e) {
+			System.out.println("Error en la base de datos.");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getInstanceConnection(String dbUrl, String dbUser, String dbPassword) {
+		if(conexion == null)
+			new Conexion(dbUrl, dbUser, dbPassword);
+		
+		return conexion;
+	}
+	
+}
